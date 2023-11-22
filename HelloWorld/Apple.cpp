015aae94efb4f56f30803bcd41c519707df2a996
Monicaco1988 <edmin.aguilaroddershede@gmail.com>
2023-11-22 21:15:26 +0100

    Randomised apple on control with round grid

--- HelloWorld/Apple.cpp
+++ HelloWorld/Apple.cpp
@@ -4,3 +4,8 @@ void Apple::Draw() // User Edmin
 {
 	Play::DrawCircle(circlePosition, 10, Play::cRed);
 };
+
+void Apple::Draw2() // User Edmin 
+{
+	Play::DrawCircle(circlePosition, 10, Play::cWhite);
+};
--- HelloWorld/Apple.h
+++ HelloWorld/Apple.h
@@ -1,15 +1,26 @@
 #pragma once
 #include <Play.h>
-
+#include <vector>
+using std::vector;
 class Apple // User: Edmin
 {
 public:
-	
+	vector <int> spawnX = { 10, 30, 50, 70, 90, 110, 130, 150, 270, 290, 310, 330, 350, 370, 390, 410, 430, 450, 470, 490, 510, 530, 550, 570, 590, 610, 630};
+	vector <int> spawnY = { 10, 30, 50, 70, 90, 110, 130, 150, 270, 290, 310, 330, 350};
+
+	//for (int k = 10; k < 660; k += 20) // x-dirr
+	//{spawnX[k] = { k }};
+
+	//for (int i = 10; i < 380; i += 20); // y-dirr
+
+	int randomPositionX = rand() % spawnX.size();
+	int randomPositionY = rand() % spawnY.size();
+
 	int x;
 	int y;
 	Point2D circlePosition;
 
-	Apple(int x, int y) // constructor 
+	Apple(const int x,const int y) // constructor kan välja värden som äpplet ska spawna på
 	{
 		this->x = x;
 		this->y = x;
@@ -18,13 +29,17 @@ public:
 
 	Apple()  // default constructor
 	{
-		this->x = rand() % (640 - 0 + 1) + 0;
-		this->y = rand() % (320 - 0 + 1) + 0;
+		this->x = spawnX[randomPositionX];
+		this->y = spawnY[randomPositionY];
 		circlePosition = { x,y };
 	};
 	
 	void Draw();
 
+	void Draw2();
+
 };
+
+//Create an array with the set x values and y values that the circles is allowed to spawn in
 
 
