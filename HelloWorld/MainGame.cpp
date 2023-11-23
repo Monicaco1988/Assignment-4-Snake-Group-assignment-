2b36e94665254158285c4a9a09a5c748daf3807c
Bjornmalmgren <frassebjorn@gmail.com>
2023-11-23 11:09:25 +0100

    Made snakepart work
    Added snakepart and made so taht i can draw a circle of diffrent colour for every snakepart

--- HelloWorld/MainGame.cpp
+++ HelloWorld/MainGame.cpp
@@ -1,6 +1,8 @@
 #define PLAY_IMPLEMENTATION
 #define PLAY_USING_GAMEOBJECT_MANAGER
 #include "Play.h"
+#include "SnakeBody.h"
+
 
 int DISPLAY_WIDTH = 640;
 int DISPLAY_HEIGHT = 360;
@@ -10,13 +12,14 @@ int DISPLAY_SCALE = 2;
 void MainGameEntry( PLAY_IGNORE_COMMAND_LINE )
 {
 	Play::CreateManager( DISPLAY_WIDTH, DISPLAY_HEIGHT, DISPLAY_SCALE );
+
 }
 
 // Called by PlayBuffer every frame (60 times a second!)
 bool MainGameUpdate( float elapsedTime )
 {
 	Play::ClearDrawingBuffer( Play::cOrange );
-	Play::DrawDebugText( { DISPLAY_WIDTH / 2, DISPLAY_HEIGHT / 2 }, "Hello World!" );
+	Play::DrawDebugText({ DISPLAY_WIDTH / 2, DISPLAY_HEIGHT / 2 }, "");
 	Play::PresentDrawingBuffer();
 	return Play::KeyDown( VK_ESCAPE );
 }
